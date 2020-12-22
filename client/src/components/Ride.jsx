d0f27cf90bda65b4eaf5e0790b6daeeb7f642af6
noam-moshe <noamoshe1234@gmail.com>
2020-12-22 17:04:10 +0200

    router

---
+++ client/src/components/Ride.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Ride = () => {
+
+
+
+
+    // const onChangeInput = (event) => {
+    //     this.setState({ input: event.target.value });
+    // }
+
+    return (
+        <div dir="ltr">
+
+            {/* {'זמן ההגעה ליעד הוא בעוד ' + ArrivalTime + 'דקות'} */}
+        </div>
+    )
+}
+
+export default Ride;
