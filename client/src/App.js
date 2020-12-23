20d5a6a8a8d224e29ecd4203e78205ed9a420191
reshitAriel <reshariel@gmail.com>
2020-12-23 09:17:35 +0200

    users list (useMemo, React.memo)

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { Route, HashRouter as Router, Switch } from 'react-router-dom';
 import Ride from './components/Ride';
+import UsersList from './components/UsersList';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Switch>
           <Route exact path='/ride' component={Ride} />
+          <Route exact path='/uses-list' component={UsersList} />
         </Switch>
       </Router>
     </div>
