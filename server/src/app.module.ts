60eacb96ecdbbec91f96401bca70830fb9d0ddce
ReutSchremer <totireut@gmail.com>
2021-04-27 18:55:50 +0300

    controllers stuff

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ServeStaticModule } from '@nestjs/serve-static'
+import { join } from 'path';
+import { GmailController } from './gmail/gmail.controller';
+import { C404, GoogleController } from './google/google.controller';
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [ServeStaticModule.forRoot({ rootPath: join(__dirname, "../../client/build") })],
+  controllers: [
+    AppController,
+    GoogleController,
+    GmailController,
+    C404
+  ],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
