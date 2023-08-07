c8b5dababf201d3ddc860043dc55c8103edcb069
shanike <shani@nadalia.com>
2023-08-07 11:51:38 +0300

    Rendi update

--- client/src/components/Rendi.js
+++ client/src/components/Rendi.js
@@ -1,21 +1,34 @@
 import React, { useEffect, useState } from 'react';
 
+
+/**
+ * This component keeps track of a `counter` according to a 1 sec interval
+ */
 export function Rendi() {
-    const [counter, setCounter] = useState(0)
+    const [counter, setCounter] = useState(0);
     const [effected, setEffected] = useState(1);
+
     useEffect(() => {
         setCounter(counter + 1);
     }, [effected])
 
     useEffect(() => {
-        setInterval(() => {
-            console.log("interval", effected)
-            console.log("new interval", effected + 1)
-            setEffected(effected + 1)
+        const interval = setInterval(() => {
+            console.log("effected", effected);
+            console.log("new effected", effected + 1);
+            setEffected((currEffected) => currEffected + 1);
         }, 1000);
+
+        return () => {
+            // cleanup:
+            clearInterval(interval);
+        }
+
     }, [])
     console.log(">> render");
-    console.log("counter: ", counter)
+    console.log("counter is: ", counter)
 
-    return <div>This component has been rendered too many times: {counter} times, affected: {effected} times</div>
+    return <div>This component has been rendered too many times:
+        <br />
+        {counter} times, affected: {effected} times</div>
 }
