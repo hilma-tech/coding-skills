a4cdafe22fb350c869598fe57bc4c1c758f0a661
shanike <shani@nadalia.com>
2023-08-07 12:19:18 +0300

    add another issue to array functions

--- client/src/common/ArrayFunctions.js
+++ client/src/common/ArrayFunctions.js
@@ -1,5 +1,5 @@
 export function ReverseArrayAndIncrease(arr) {
-    return arr.reverse().map(item => item++).map(item => console.log(item));
+    return arr.reverse().map(item => item++).filter(item => item > 0).map(item => console.log(item));
 }
 
 export async function AsyncArr(array) {
